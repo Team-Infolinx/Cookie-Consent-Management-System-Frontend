88dc944cd10171c5f7c28466589174430762b1db
lakshankd <dominiclakshank@gmail.com>
2023-04-18 23:00:41 +0530

    adding folder structure.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Analytics from "./pages/Analytics";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import WebsiteForm from "./pages/WebsiteForm";
+import Websites from "./pages/Websites";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Route>
+        <Route path="/user" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="websites" element={<Websites />} />
+          <Route path="websiteform" element={<WebsiteForm />} />
+          <Route path="analytics" element={<Analytics />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/WebsiteForm.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function WebsiteForm() {
+  return <div>WebsiteForm</div>;
+}
