9f1434feb9f9bfd931bd34b643d0deace8851e36
Dominic Lakshan <dominiclakshank@gmail.com>
2023-04-26 21:03:12 +0530

    update asgardeo configurations.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <AuthProvider
         config={ {
-            signInRedirectURL: "http://localhost:3000",
+            signInRedirectURL: "http://localhost:3000/user",
             signOutRedirectURL: "http://localhost:3000",
             clientID: "mggwJwJ0Wob9M161uzaWA8Yl1Pga",
             baseUrl: "https://api.asgardeo.io/t/org80k8n",
