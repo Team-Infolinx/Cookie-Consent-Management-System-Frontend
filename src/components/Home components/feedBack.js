92d7b682be559ec671f78868921e79ae46c1cd6e
DINUSHA26 <manidudinusha@gmail.com>
2023-06-08 01:14:34 +0530

    adding homepage changes

--- src/components/Home components/feedBack.js
+++ src/components/Home components/feedBack.js
@@ -1,9 +1,9 @@
-import {Typography,Avatar} from "@mui/material";
+import { Typography, Avatar } from "@mui/material";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper";
 import Grid from "@mui/material/Grid";
-import "./feedback-styles.css"
-import 'swiper/swiper-bundle.min.css';
+import "./feedback-styles.css";
+import "swiper/swiper-bundle.min.css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
@@ -11,69 +11,122 @@ import cus1 from "../../assets/image/customer1.jpg";
 import cus2 from "../../assets/image/customer2.jpg";
 import cus3 from "../../assets/image/customer 3.avif";
 
-
-
 const CustomerFeedBack = () => {
+  const feedbackItems = [
+    {
+      id: "0",
+      name: "Jane",
+      image: cus1,
+      text:
+        "I was really impressed with how easy it was to set up and customize the cookie consent banner on my website using your system. The analytics  " +
+        "are also really useful for keeping track of my website's cookie usage. " +
+        "Thank you for such a great product!",
+    },
+    {
+      id: "1",
+      name: "John",
+      image: cus2,
+      text:
+        "I was really impressed with how easy it was to set up and customize the cookie consent banner on my website using your system. The analytics  " +
+        "are also really useful for keeping track of my website's cookie usage. " +
+        "Thank you for such a great product!",
+    },
+    {
+      id: "2",
+      name: "Jack",
+      image: cus3,
+      text:
+        "I was really impressed with how easy it was to set up and customize the cookie consent banner on my website using your system. The analytics  " +
+        "are also really useful for keeping track of my website's cookie usage. " +
+        "Thank you for such a great product!",
+    },
+  ];
 
+  return (
+    <div
+      className="customerFeedBack"
+      style={{
+        backgroundColor: "#ffffff",
+        backgroundRepeat: "no-repeat",
+        minHeight: "80vh",
+      }}
+    >
+      <Grid>
+        <Typography
+          style={{ color: "#024481" }}
+          variant="h2"
+          pt={10}
+          pl={5}
+          pb={5}
+        >
+          What our customers say
+        </Typography>
+      </Grid>
 
+      <Grid
+        container
+        direction={"column"}
+        justify={"center"}
+        alignItems={"center"}
+        lg={12}
+        md={12}
+        sm={12}
+        xs={12}
+        sx={{ height: "50px" }}
+      />
 
-    const feedbackItems = [
-        {id:"0",name: "Jane", image:cus1, text:"I was really impressed with how easy it was to set up and customize the cookie consent banner on my website using your system. The analytics  " +
-                "are also really useful for keeping track of my website's cookie usage. " +
-                "Thank you for such a great product!"},
-        {id:"1",name: "John", image: cus2, text:"I was really impressed with how easy it was to set up and customize the cookie consent banner on my website using your system. The analytics  " +
-                "are also really useful for keeping track of my website's cookie usage. " +
-                "Thank you for such a great product!"},
-        {id:"2",name: "Jack", image: cus3, text:"I was really impressed with how easy it was to set up and customize the cookie consent banner on my website using your system. The analytics  " +
-                "are also really useful for keeping track of my website's cookie usage. " +
-                "Thank you for such a great product!"},
-
-
-    ];
-
-    return (
-        <div className="customerFeedBack"
-             style={{
-                 backgroundColor:'#ffffff',
-                 backgroundRepeat: "no-repeat",
-                 minHeight: "80vh",
-
-             }}>
-            <Grid>
-                <Typography style={{color : "#024481"}} variant="h2" pt={10} pl={5} pb={5}>What our customers say</Typography>
-            </Grid>
-
-            <Grid container direction={"column"} justify={"center"}  alignItems={"center"} lg={12} md={12} sm={12} xs={12} sx={{height:"50px"}} />
-
-            <Grid alignItems={"center"} className={"swiper-slide"}>
-                <Swiper
-                    navigation={true}
-                    pagination={{
-                        dynamicBullets: true,
-                    }}
-                    modules={[Pagination, Navigation]}
-                    style={{width: "80vw"}}
+      <Grid alignItems={"center"} className={"swiper-slide"}>
+        <Swiper
+          navigation={true}
+          pagination={{
+            dynamicBullets: true,
+          }}
+          modules={[Pagination, Navigation]}
+          style={{ width: "80vw" }}
+        >
+          {feedbackItems.map((feedback) => (
+            <SwiperSlide key={feedback.id}>
+              <Grid container direction={"row"} justifyContent={"space-evenly"}>
+                <Grid
+                  container
+                  item
+                  direction={"column"}
+                  lg={6}
+                  justifyContent={"center"}
+                  alignContent={"center"}
                 >
-                    {feedbackItems.map((feedback) => (
-                        <SwiperSlide key={feedback.id} >
-                                    <Grid container direction={"row"} justifyContent={"space-evenly"}>
-                                        <Grid container item direction={"column"} lg={6} justifyContent={"center"} alignContent={"center"} >
-                                            <Avatar alt="Customer image" src={feedback.image} sx={{width: 300, height: 300}}  />
-                                        </Grid>
-                                        <Grid container item direction={"column"} lg={6} justifyContent={"center"} flexWrap={"nowrap"}>
-                                            <Typography variant="h4" component="div">
-                                                {feedback.name}
-                                            </Typography>
-                                            <Typography variant="body1" color="text.secondary">
-                                                {feedback.text}
-                                            </Typography>
-                                        </Grid>
-                                    </Grid>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </Grid>
-        </div> );
-}
+                  <Avatar
+                    alt="Customer image"
+                    src={feedback.image}
+                    sx={{ width: 300, height: 300 }}
+                  />
+                </Grid>
+                <Grid
+                  container
+                  item
+                  direction={"column"}
+                  lg={6}
+                  justifyContent={"center"}
+                  flexWrap={"nowrap"}
+                >
+                  <Typography variant="h4" component="div">
+                    {feedback.name}
+                  </Typography>
+                  <Typography
+                    variant="body1"
+                    color="text.secondary"
+                    sx={{ paddingRight: 10 }}
+                  >
+                    {feedback.text}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Grid>
+    </div>
+  );
+};
 
 export default CustomerFeedBack;
--- src/components/Home components/footer.js
+++ src/components/Home components/footer.js
@@ -1,84 +1,169 @@
-import {Button,Typography} from '@mui/material';
-import Grid from '@mui/material/Grid';
+import { Button, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
 import logoWhite from "../../assets/svg/web-safe-logo - white.svg";
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import DraftsOutlinedIcon from '@mui/icons-material/DraftsOutlined';
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import {HashLink} from "react-router-hash-link";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import DraftsOutlinedIcon from "@mui/icons-material/DraftsOutlined";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import { HashLink } from "react-router-hash-link";
 
 const Footer = () => {
-    return ( 
-    <div className="footer"
-    style={{
-                backgroundColor:'#00a5ff',
-                backgroundRepeat: "no-repeat",
-
-            }}>
-
-        <Grid container direction="row" justifyContent="space-evenly" alignItems="center">
-            
-            {/*Column 1 */}
-            <Grid container direction="column" justify={"center"} alignItems={"center"} item lg={4} md={12} sm={12}
-                      xs={12}  >
-                <div className="white logo" style={{paddingLeft:"10px"}}>
-                    <img src={logoWhite}
-                            alt={"logo white"}
-                            style={{
-                                height: 200,
-                                width: 400,
-                                marginRight: '1em',
-                            }}></img></div>
-                <Typography style={{ color: "#ffffff" }} variant="h5">About us</Typography>
-                <Typography style={{ color: "#ffffff" }} variant="h6">We manage your consent</Typography>
-                <ul className='social icons'style={{display:'flex', justifyContent:'space-between', listStyleType:'none',paddingRight:"40px" }}>
-                    <li style={{width:"30%", textAlign:"center", color:"#ffffff"}}><FacebookIcon /></li>
-                    <li style={{width:"30%", textAlign:"center",color:"#ffffff"}}><InstagramIcon /></li>
-                    <li style={{width:"30%", textAlign:"center",color:"#ffffff"}}><LinkedInIcon /></li>
-                    <li style={{width:"30%", textAlign:"center",color:"#ffffff"}}><TwitterIcon /></li>
-                </ul>
-            </Grid>
-
-
-            {/*Column 2.*/}
-            <Grid container direction="column" justify={"center"} alignItems={"center"} item lg={4} md={12} sm={12}
-                      xs={12}>
-                <Typography variant={"h5"} style={{ color: "#ffffff" }}>Useful links</Typography>
-                <HashLink smooth to={'/#homeSection_1'} style={{ textDecoration: 'none' }}>
-                    <Button variant={"text"}>
-                        <Typography variant={"h6"} style={{ color: "#ffffff" }}>Home</Typography>
-                    </Button>
-                </HashLink>
-                <HashLink smooth to={'/#homeSection_2'}  style={{ textDecoration: 'none' }}>
-                    <Button variant={"text"}>
-                        <Typography variant={"h6"} style={{ color: "#ffffff" }}>Features</Typography>
-                    </Button>
-                </HashLink>
-                <HashLink smooth to={'/#homeSection_3'}  style={{ textDecoration: 'none' }}>
-                    <Button variant={"text"}>
-                        <Typography variant={"h6"} style={{ color: "#ffffff" }}>About Us</Typography></Button>
-                </HashLink>
-            </Grid>
-            
-
-            {/*Column 3.*/}
-            <Grid container direction="column" justify={"center"} alignItems={"center"} item lg={4} md={12} sm={12} xs={12}>
-            <Typography variant={"h5"} style={{ color: "#ffffff" }}>Contact with us</Typography>
-            <Typography variant={"h6"} style={{ color: "#ffffff" }}><DraftsOutlinedIcon />WebSafe@gmail.com</Typography>
-            <Typography variant={"h6"} style={{ color: "#ffffff" }}><LocalPhoneIcon />+9400-000-0000</Typography>
-            </Grid>
-
+  return (
+    <div
+      className="footer"
+      style={{
+        backgroundColor: "#00a5ff",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-evenly"
+        alignItems="center"
+      >
+        {/*Column 1 */}
+        <Grid
+          container
+          direction="column"
+          justify={"center"}
+          alignItems={"center"}
+          item
+          lg={4}
+          md={12}
+          sm={12}
+          xs={12}
+        >
+          <div className="white logo" style={{ paddingLeft: "15px" }}>
+            <img
+              src={logoWhite}
+              alt={"logo white"}
+              style={{
+                height: 200,
+                width: 400,
+                marginRight: "1em",
+              }}
+            ></img>
+          </div>
+          <Typography style={{ color: "#ffffff" }} variant="h5">
+            About us
+          </Typography>
+          <Typography style={{ color: "#ffffff" }} variant="h6">
+            We manage your consent
+          </Typography>
+          <ul
+            className="social icons"
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              listStyleType: "none",
+              paddingRight: "40px",
+            }}
+          >
+            <li style={{ width: "30%", textAlign: "center", color: "#ffffff" }}>
+              <FacebookIcon />
+            </li>
+            <li style={{ width: "30%", textAlign: "center", color: "#ffffff" }}>
+              <InstagramIcon />
+            </li>
+            <li style={{ width: "30%", textAlign: "center", color: "#ffffff" }}>
+              <LinkedInIcon />
+            </li>
+            <li style={{ width: "30%", textAlign: "center", color: "#ffffff" }}>
+              <TwitterIcon />
+            </li>
+          </ul>
         </Grid>
-        <Grid container direction="row" justifyContent={"center"}
-                  style={{backgroundColor : "#024481", padding: "30px"}}>
-                <Typography style={{textAlign:"center", color:"#ffffff"}}>Copyright Web Safe © 2023. All rights reserved.
-                </Typography>
+
+        {/*Column 2.*/}
+        <Grid
+          container
+          direction="column"
+          justify={"center"}
+          alignItems={"center"}
+          item
+          lg={4}
+          md={12}
+          sm={12}
+          xs={12}
+        >
+          <Typography variant={"h5"} style={{ color: "#ffffff" }}>
+            Useful links
+          </Typography>
+          <HashLink
+            smooth
+            to={"/#homeSection_1"}
+            style={{ textDecoration: "none" }}
+          >
+            <Button variant={"text"}>
+              <Typography variant={"h6"} style={{ color: "#ffffff" }}>
+                Home
+              </Typography>
+            </Button>
+          </HashLink>
+          <HashLink
+            smooth
+            to={"/#homeSection_2"}
+            style={{ textDecoration: "none" }}
+          >
+            <Button variant={"text"}>
+              <Typography variant={"h6"} style={{ color: "#ffffff" }}>
+                Features
+              </Typography>
+            </Button>
+          </HashLink>
+          <HashLink
+            smooth
+            to={"/#homeSection_3"}
+            style={{ textDecoration: "none" }}
+          >
+            <Button variant={"text"}>
+              <Typography variant={"h6"} style={{ color: "#ffffff" }}>
+                About Us
+              </Typography>
+            </Button>
+          </HashLink>
         </Grid>
-    </div> );
-}
 
+        {/*Column 3.*/}
+        <Grid
+          container
+          direction="column"
+          justify={"center"}
+          alignItems={"center"}
+          item
+          lg={4}
+          md={12}
+          sm={12}
+          xs={12}
+        >
+          <Typography variant={"h5"} style={{ color: "#ffffff" }}>
+            Contact with us
+          </Typography>
+          <Typography variant={"h6"} style={{ color: "#ffffff" }}>
+            <DraftsOutlinedIcon />
+            WebSafe@gmail.com
+          </Typography>
+          <Typography variant={"h6"} style={{ color: "#ffffff" }}>
+            <LocalPhoneIcon />
+            +9400-000-0000
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        direction="row"
+        justifyContent={"center"}
+        style={{ backgroundColor: "#024481", padding: "15px" }}
+      >
+        <Typography style={{ textAlign: "center", color: "#ffffff" }}>
+          Copyright Web Safe © 2023. All rights reserved.
+        </Typography>
+      </Grid>
+    </div>
+  );
+};
 
- 
 export default Footer;
