55d7417e9c2e978ffb0f7b508cbb9c2035de20f8
HeshanSnjaya <heshansnjaya@gmail.com>
2023-06-14 05:58:30 +0530

    adding endpoints to frontend

--- src/pages/Analytics.js
+++ src/pages/Analytics.js
@@ -1,17 +1,45 @@
-import { Card, FormControl, Grid, MenuItem, Select } from "@mui/material";
+import {
+  Alert,
+  Card,
+  FormControl,
+  Grid,
+  MenuItem,
+  Select,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import AppWidget from "../common components/AppWidget";
 import AppBarChart from "../common components/AppBarChart";
 import axios from "axios";
 import AppPieChart from "../common components/AppPieChart";
+import { useAuthContext } from "@asgardeo/auth-react";
 
 export default function Dashboard() {
-  const [noOfCookies, setNoOfCookies] = useState([]);
-  const [noOfConsent, setNoOfConsent] = useState([]);
-  const [noOfAcceptedConsent, setNoOfAcceptedConsent] = useState([]);
-  const [acceptanceRate, setAcceptanceRate] = useState([]);
+  const [noOfCookies, setNoOfCookies] = useState([0]);
+  const [noOfConsent, setNoOfConsent] = useState([0]);
+  const [noOfAcceptedConsent, setNoOfAcceptedConsent] = useState([0]);
+  const [acceptanceRate, setAcceptanceRate] = useState([0]);
   const [websites, setWebsites] = useState([]);
   const [selectedWebsiteId, setselectedWebsiteId] = useState("");
+  const [websitevisitdate, setWebsitevisitdate] = useState([]);
+  const [noOfWebsiteVisits, setNoOfWebsiteVisits] = useState([]);
+
+  const { getDecodedIDToken } = useAuthContext();
+  const { state } = useAuthContext();
+  const [userId, setUserId] = useState("");
+
+  useEffect(() => {
+    if (state.isAuthenticated) {
+      getDecodedIDToken()
+        .then((decodedIDToken) => {
+          setUserId(decodedIDToken.sub);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [state]);
+
+  console.log("This is user Id" + userId);
 
   const handleChange = (event) => {
     setselectedWebsiteId(event.target.value);
@@ -19,17 +47,16 @@ export default function Dashboard() {
     loadNoOfConsent(event.target.value);
     loadNoOfAcceptedConsent(event.target.value);
     loadAcceptanceRate(event.target.value);
-    loadWebsites(event.target.value);
+    loadWebsites(userId);
+    loadWebsiteVisitDates(event.target.value);
+    loadNoOfWebsiteVisits(event.target.value);
   };
 
   useEffect(() => {
-    setselectedWebsiteId(1);
-    loadNoOfCookies(1);
-    loadNoOfConsent(1);
-    loadNoOfAcceptedConsent(1);
-    loadAcceptanceRate(1);
-    loadWebsites(1);
-  }, []);
+    if (userId) {
+      loadWebsites(userId);
+    }
+  }, [userId]);
 
   const loadNoOfCookies = async (id) => {
     const result = await axios.get(
@@ -61,104 +88,132 @@ export default function Dashboard() {
 
   const loadWebsites = async (id) => {
     const result = await axios.get(
-      "http://localhost:8080/api/v1/user/getWebsites"
+      `http://localhost:8080/api/v1/users/${id}/websites`
     );
     setWebsites(result.data);
+    console.log(JSON.stringify(result.data));
+  };
+
+  const loadWebsiteVisitDates = async (id) => {
+    const result = await axios.get(
+      "http://localhost:8080/api/v1/user/getWebsiteVisitsdates/" + id
+    );
+    setWebsitevisitdate(result.data);
+    console.log("date list" + result.data);
+  };
+
+  const loadNoOfWebsiteVisits = async (id) => {
+    const result = await axios.get(
+      "http://localhost:8080/api/v1/user/getWebsiteVisitsCount/" + id
+    );
+    setNoOfWebsiteVisits(result.data);
   };
 
   return (
     <div>
-      <Grid container spacing={2} sx={{ pt: 1 }}>
-        <Grid item xs={12} sm={4} md={4}>
-          <Card
-            sx={{
-              py: 0.5,
-              boxShadow: 2,
-              textAlign: "center",
-              borderRadius: "10px",
-              backgroundColor: "primary.danger",
-              "&:hover": {
-                backgroundColor: "primary.dark",
-                opacity: [0.9, 0.8, 0.7],
-              },
-            }}
-          >
-            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-              <Select
-                labelId="website-label"
-                id="website"
-                value={selectedWebsiteId}
-                autoWidth
-                label="website"
-                onChange={handleChange}
+      {websites.length === 0 ? (
+        <Alert severity="info">
+          There are no included websites at the moment. You can add new websites
+          by clicking the 'Add Website' button.
+        </Alert>
+      ) : (
+        <div>
+          <Grid container spacing={2} sx={{ pt: 1 }}>
+            <Grid item xs={12} sm={4} md={4}>
+              <Card
+                sx={{
+                  py: 0.5,
+                  boxShadow: 2,
+                  textAlign: "center",
+                  borderRadius: "10px",
+                  backgroundColor: "primary.danger",
+                  "&:hover": {
+                    backgroundColor: "primary.dark",
+                    opacity: [0.9, 0.8, 0.7],
+                  },
+                }}
               >
-                {websites.map((website) => (
-                  <MenuItem value={website.websiteId}>
-                    {website.configName}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Card>
-        </Grid>
-      </Grid>
-      <Grid container spacing={3} sx={{ pt: 5 }}>
-        <Grid item xs={12} sm={6} md={3}>
-          <AppWidget value={noOfCookies} title={"Cookies"} />
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <AppWidget value={noOfConsent} title={"Interacted Users"} />
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <AppWidget value={noOfAcceptedConsent} title={"Accepted Users"} />
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <AppWidget value={acceptanceRate + "%"} title={"Acceptance Rate"} />
-        </Grid>
-      </Grid>
+                <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                  <Select
+                    labelId="website-label"
+                    id="website"
+                    value={selectedWebsiteId}
+                    autoWidth
+                    label="website"
+                    onChange={handleChange}
+                  >
+                    {websites.map((website) => (
+                      <MenuItem value={website.websiteId}>
+                        {website.domain}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Card>
+            </Grid>
+          </Grid>
+          <Grid container spacing={3} sx={{ pt: 5 }}>
+            <Grid item xs={12} sm={6} md={3}>
+              <AppWidget value={noOfCookies} title={"Cookies"} />
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <AppWidget value={noOfConsent} title={"Interacted Users"} />
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <AppWidget value={noOfAcceptedConsent} title={"Accepted Users"} />
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <AppWidget
+                value={acceptanceRate + "%"}
+                title={"Acceptance Rate"}
+              />
+            </Grid>
+          </Grid>
 
-      <Grid container spacing={3} sx={{ pt: 5 }}>
-        <Grid item xs={12} md={12} lg={8}>
-          <AppBarChart
-            title="Website Visits"
-            subheader="(+43%) than last year"
-            chartLabels={[
-              "01/01/2003",
-              "02/01/2003",
-              "03/01/2003",
-              "04/01/2003",
-              "05/01/2003",
-              "06/01/2003",
-              "07/01/2003",
-              "08/01/2003",
-              "09/01/2003",
-              "10/01/2003",
-              "11/01/2003",
-              "12/01/2003",
-            ]}
-            chartData={[
-              {
-                name: "Website A",
-                type: "column",
-                fill: "solid",
-                data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
-              },
-            ]}
-          />
-        </Grid>
-        <Grid item xs={12} md={12} lg={4}>
-          <AppPieChart
-            title="Acceptance Rate on cookie types"
-            chartData={[
-              { label: "Functional", value: 4344 },
-              { label: "Performance", value: 5435 },
-              { label: "Target", value: 1443 },
-              { label: "Other", value: 4443 },
-            ]}
-            chartColors={["#ff4842", "#fec107", "#1790ff", "#00ab55"]}
-          />
-        </Grid>
-      </Grid>
+          <Grid container spacing={3} sx={{ pt: 5 }}>
+            <Grid item xs={12} md={12} lg={8}>
+              <AppBarChart
+                title="Website Visits"
+                subheader="(+43%) than last year"
+                chartLabels={[
+                  "01/01/2023",
+                  "02/01/2023",
+                  "03/01/2023",
+                  "04/01/2023",
+                  "05/01/2023",
+                  "06/01/2023",
+                  "07/01/2023",
+                  "08/01/2023",
+                  "09/01/2023",
+                  "10/01/2023",
+                  "11/01/2023",
+                  "12/01/2023",
+                ]}
+                chartData={[
+                  {
+                    name: "Website A",
+                    type: "line",
+                    fill: "solid",
+                    data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
+                  },
+                ]}
+              />
+            </Grid>
+            <Grid item xs={12} md={12} lg={4}>
+              <AppPieChart
+                title="Acceptance Rate on cookie types"
+                chartData={[
+                  { label: "Functional", value: 4344 },
+                  { label: "Performance", value: 5435 },
+                  { label: "Target", value: 1443 },
+                  { label: "Other", value: 4443 },
+                ]}
+                chartColors={["#ff4842", "#fec107", "#1790ff", "#00ab55"]}
+              />
+            </Grid>
+          </Grid>
+        </div>
+      )}
     </div>
   );
 }
